8b4bc214de3d7832cc17299250c3341d7faac7d8
KercyDing <dkx215417@gmail.com>
2026-05-01 06:23:37 +0800

    feat: simple character printing

--- build.zig
+++ build.zig
@@ -1,25 +1,39 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
+    const target = b.standardTargetOptions(.{
+        .default_target = .{
+            .cpu_arch = .riscv64,
+            .os_tag = .freestanding,
+            .abi = .none,
+        },
+    });
     const optimize = b.standardOptimizeOption(.{});
     const strip = b.option(bool, "strip", "Strip debug info") orelse false;
 
-    const exe = b.addExecutable(.{
+    const kernel = b.addExecutable(.{
         .name = "krcyos",
         .root_module = b.createModule(.{
             .root_source_file = b.path("os/main.zig"),
                                       .target = target,
                                       .optimize = optimize,
                                       .strip = strip,
+                                      .code_model = .medany,
         }),
     });
+    kernel.setLinkerScript(b.path("os/linker.ld"));
+    b.installArtifact(kernel);
 
-    b.installArtifact(exe);
+    const qemu_cmd = b.addSystemCommand(&.{
+        "qemu-system-riscv64",
+        "-machine", "virt",
+        "-nographic",
+        "-bios", "bootloader/opensbi.bin",
+        "-kernel",
+    });
+    qemu_cmd.addArtifactArg(kernel);
+    qemu_cmd.step.dependOn(b.getInstallStep());
 
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    const run_step = b.step("run", "Run the kernel in QEMU");
+    run_step.dependOn(&qemu_cmd.step);
 }
--- os/main.zig
+++ os/main.zig
@@ -1,5 +1,53 @@
 const std = @import("std");
 
-pub fn main() void {
-    std.debug.print("This is KrcyOS:-)\n", .{});
+extern var sbss: u8;
+extern var ebss: u8;
+extern var boot_stack_top: u8;
+
+export fn _start() linksection(".text.entry") callconv(.naked) noreturn {
+    asm volatile (
+        \\la sp, boot_stack_top
+        \\call kmain
+    );
+    while (true) {}
 }
+
+/// Kernel main function
+export fn kmain() noreturn {
+    clearBss();
+
+    const hello = "Hello, KrcyOS from Zig!\n";
+    for (hello) |char| {
+        consolePutchar(char);
+    }
+
+    while (true) {}
+}
+
+/// Clear bss function
+fn clearBss() void {
+    var start: [*]u8 = @ptrCast(&sbss);
+    const end: [*]u8 = @ptrCast(&ebss);
+    while (@intFromPtr(start) < @intFromPtr(end)) {
+        start[0] = 0;
+        start += 1;
+    }
+}
+
+// Yes, if you write it in pure asm, it would be like:
+//
+// li a7, 1
+// li a6, 0
+// ecall
+// ret
+//
+fn consolePutchar(char: u8) void {
+    _ = asm volatile ("ecall"
+        : [ret] "={a0}" (-> usize)
+        : [eid] "{a7}" (0x01),
+          [fid] "{a6}" (0),
+          [arg0] "{a0}" (char)
+    );
+}
+
+export var boot_stack: [4096 * 4]u8 align(16) linksection(".bss.stack") = undefined;
